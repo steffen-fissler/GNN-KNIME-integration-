9cbc68d96cef1f8850e00f333df1275767aa0757
Victor <49013992+Victor-Palacios@users.noreply.github.com>
2023-04-11 17:42:37 -0700

    Minor and Major Changes
    Minor changes: naming, reduction of unused variables
    Major changes: used execution context to generate progression bar in KNIME, added SAGEConv layer as an option so that our node name is now GNN instead of GCN, other changes to code for readability and/or usability.

--- my_extension.py
+++ my_extension.py
@@ -4,7 +4,7 @@ import logging
 
 LOGGER = logging.getLogger(__name__)
 
-@knext.node(name="GCN Graph Creator", node_type=knext.NodeType.MANIPULATOR, icon_path="icon.png", category="/")
+@knext.node(name="GNN Graph Creator", node_type=knext.NodeType.MANIPULATOR, icon_path="icon.png", category="/")
 @knext.input_table(name="Nodes with Features", description="Give a table of ndoes with features and target.")
 @knext.input_table(name="Edges", description="Give two columns: one with a node and second column with the paired node")
 @knext.output_binary(
@@ -12,7 +12,7 @@ LOGGER = logging.getLogger(__name__)
     description="Pytorch Geometric Graph",
     id="org.knime.torch.graphcreator")
 # @knext.output_table("Target Data", "Used for debugging") # DEBUGGING CODE
-class GCNCreator:
+class GNNCreator:
     """
     Take two tables to construct a full graph. 
     """
@@ -36,7 +36,8 @@ class GCNCreator:
         
         g = self.construct_graph(nodes_data=nodes_data,
                                  edges_data=edges_data,
-                                 exec=knext.ExecutionContext)
+                                )
+
         graph_dict = {'graph':g,
                       'num_of_class':num_class,
                       'key': self.key}
@@ -48,7 +49,7 @@ class GCNCreator:
         # output_table = pa.table([pa.array(labels)], names=["Target"])
         # return knext.Table.from_pyarrow(output_table)
     
-    def construct_graph(self, nodes_data, edges_data, exec:knext.ExecutionContext):
+    def construct_graph(self, nodes_data, edges_data):
         node_features_list = nodes_data.drop(columns=[self.key,self.target]).values.tolist()
         node_features = torch.tensor(node_features_list)
         node_labels = torch.tensor(nodes_data[self.target].values)
@@ -63,7 +64,7 @@ class GCNCreator:
 
 
 
-@knext.node(name="GCN Learner", node_type=knext.NodeType.LEARNER, icon_path="icon.png", category="/")
+@knext.node(name="GNN Learner", node_type=knext.NodeType.LEARNER, icon_path="icon.png", category="/")
 @knext.input_binary("Full Graph", "Pickled Graph", "org.knime.torch.graphcreator" )
 @knext.input_table(name="Train Set", description="A table that contains nodes need to be masked and passed on.")
 @knext.output_binary(
@@ -71,12 +72,13 @@ class GCNCreator:
     description="Pytorch Geometric Model",
     id="org.knime.torch.learner",
 )
-class GCNLearner:
+class GNNLearner:
     """
     """
     #TODO Make validation accuracy be in output of the learner node 
     learning_rate = knext.DoubleParameter("Learning Rate", "The learning rate to use in the optimizer", 0.1, min_value=1e-10)
     epochs = knext.IntParameter("Epochs", "The number of epochs to train for.", 1)
+    convolutional_layer = knext.StringParameter("Convolutional Layer Choice", "The convolutional layer to be used in the neural network.", "GCNConv", enum=["GCNConv", "SAGEConv"])
     criterion = nn.CrossEntropyLoss()
 
     def configure(self, configure_context, input_binary_1, input_schema_1):
@@ -92,32 +94,64 @@ class GCNLearner:
 
         num_class = graph_dict['num_of_class']
         num_of_feat = masked_graph.num_node_features
-        model = SocialGNN(num_of_feat=num_of_feat,
-                          hidden_layer=16,
-                          num_class=num_class)
+        model = GNN(num_of_feat=num_of_feat,
+                    hidden_layer=16,
+                    num_class=num_class,
+                    convolutional_layer=self.convolutional_layer)
 
-        train_accuracies, buffer = self.train_social(model, masked_graph, knext.ExecutionContext)
+        # #### BEGIN TRAINING ####
+        # epochs = self.epochs
+        # lr = self.learning_rate
+        # optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        
+        # train_losses=[]
+        # train_accuracies=[]
+        
+        # for e in range(epochs+1):
+        #     exec_context.set_progress(progress=e/epochs, message=f"Training {e} for {epochs}")
+        #     if exec_context.is_canceled():
+        #         raise RuntimeError("Execution terminated by user")
+        #     optimizer.zero_grad()
+        #     out=model(masked_graph)
+        #     loss=self.masked_loss(predictions=out,
+        #                           labels=masked_graph.y,
+        #                           mask=masked_graph.data_mask)
+        #     loss.backward()
+        #     optimizer.step()
+        #     train_losses+=[loss]
+        #     train_accuracy=self.masked_accuracy(predictions=out,
+        #                                         labels=masked_graph.y, 
+        #                                         mask=masked_graph.data_mask)
+        #     train_accuracies+=[float(train_accuracy)] # requires float for pyarrow
+
+        # state_dict = model.state_dict()
+        # buffer = BytesIO()
+        # torch.save(state_dict, buffer)
+        # buffer.seek(0)
+        # #### END TRAINING ####
+
+        train_accuracies, buffer = self.train(model, masked_graph, exec_context)
 
         model_dict = {"model": buffer.read(),
                       "data": graph,
                       "train_accuracies": train_accuracies,
                       "num_of_feat": num_of_feat,
                       "num_of_class": num_class,
-                      "key": graph_dict["key"]}
+                      "key": graph_dict["key"],
+                      "convolutional_layer": self.convolutional_layer}
         # statistics_table = pa.table([pa.array([str(i) for i in range(len(train_accuracies))]), pa.array(train_accuracies)], names=["Train Accuracy"])
         
         return pickle.dumps(model_dict)#, knext.Table.from_pyarrow(statistics_table)
 
-    def masked_loss(self, predictions, labels, mask, exec: knext.ExecutionContext):
+    def masked_loss(self, predictions, labels, mask):
         mask=mask.float()
         mask=mask/torch.mean(mask)
-        # had to add the type longtensor 
         loss=self.criterion(predictions,labels.type(torch.LongTensor))
         loss=loss*mask
         loss=torch.mean(loss)
         return (loss)    
 
-    def masked_accuracy(self, predictions, labels, mask, exec: knext.ExecutionContext):
+    def masked_accuracy(self, predictions, labels, mask):
         mask=mask.float()
         mask/=torch.mean(mask)
         accuracy=(torch.argmax(predictions,axis=1)==labels).long()
@@ -125,7 +159,7 @@ class GCNLearner:
         accuracy=torch.mean(accuracy)
         return (accuracy)    
 
-    def train_social(self, model, g, exec: knext.ExecutionContext):
+    def train(self, model, g, exec_context):
         epochs = self.epochs
         lr = self.learning_rate
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
@@ -133,20 +167,21 @@ class GCNLearner:
         train_losses=[]
         train_accuracies=[]
         
-        for ep in range(epochs+1):
+        for e in range(epochs+1):
+            exec_context.set_progress(progress=e/epochs, message=f"Training {e} for {epochs}")
+            if exec_context.is_canceled():
+                raise RuntimeError("Execution terminated by user")
             optimizer.zero_grad()
             out=model(g)
             loss=self.masked_loss(predictions=out,
                                   labels=g.y,
-                                  mask=g.data_mask,
-                                  exec=knext.ExecutionContext)
+                                  mask=g.data_mask)
             loss.backward()
             optimizer.step()
             train_losses+=[loss]
             train_accuracy=self.masked_accuracy(predictions=out,
                                                 labels=g.y, 
-                                                mask=g.data_mask,
-                                                exec=knext.ExecutionContext)
+                                                mask=g.data_mask)
             train_accuracies+=[float(train_accuracy)] # requires float for pyarrow
 
         state_dict = model.state_dict()
@@ -158,11 +193,11 @@ class GCNLearner:
 
 
 
-@knext.node(name="GCN Predictor", node_type=knext.NodeType.PREDICTOR, icon_path="icon.png", category="/")
+@knext.node(name="GNN Predictor", node_type=knext.NodeType.PREDICTOR, icon_path="icon.png", category="/")
 @knext.input_binary("GNN model", "The trained model", "org.knime.torch.learner")
 @knext.input_table(name="Test Set", description="A table that contains nodes for testing.")
 @knext.output_table("Table with Prediction", "Append prediction probability and class to table")
-class GCNPredictor:
+class GNNPredictor:
     """
     """
     prediction_confidence = knext.BoolParameter("Append overall prediction confidence", "Probability of being the 1 class", False)
@@ -184,11 +219,13 @@ class GCNPredictor:
         num_of_feat = model_dict["num_of_feat"]
         num_class = model_dict["num_of_class"]
         msk = AddMask(model_dict["key"])
+        convolutional_layer = model_dict["convolutional_layer"]
         masked_graph = msk(graph, test_set)
 
-        model = SocialGNN(num_of_feat=num_of_feat,
-                          hidden_layer = 16,
-                          num_class=num_class)
+        model = GNN(num_of_feat=num_of_feat,
+                    hidden_layer=16,
+                    num_class=num_class,
+                    convolutional_layer=convolutional_layer)
 
         state_dict = torch.load(BytesIO(model_dict["model"]))
         model.load_state_dict(state_dict)
