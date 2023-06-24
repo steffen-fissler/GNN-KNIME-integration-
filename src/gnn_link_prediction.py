5f77db42b80f5e27b4dddeed3395b986f50f5c4a
sunshineccl <jinwei.k.sun@gmail.com>
2023-06-23 18:04:24 -0700

    Fromat update
    Updated format for coding and file structure

--- src/gnn_link_prediction.py
+++ src/gnn_link_prediction.py
@@ -1,12 +1,12 @@
 from tts import *
-from link_prediction import *
-import knime_extension as knext
+from gnn_link_prediction import *
+import knime.extension as knext
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
 @knext.node(name="GNN Graph Creator", node_type=knext.NodeType.MANIPULATOR, 
-            icon_path="icon.png", category="/")
+            icon_path="../icons/icon.png", category="/")
 @knext.input_table(name="Nodes with Features", 
                    description="Give a table of nodes with features and target.")
 @knext.input_table(name="Edges", 
@@ -81,7 +81,7 @@ class GNNCreator:
         return(g)
 
 
-@knext.node(name="GNN Learner", node_type=knext.NodeType.LEARNER, icon_path="icon.png", category="/")
+@knext.node(name="GNN Learner", node_type=knext.NodeType.LEARNER, icon_path="../icons/icon.png", category="/")
 @knext.input_binary("Full Graph", "Pickled Graph", "org.knime.torch.graphcreator" )
 @knext.input_table(name="Train Set", 
                    description="A table that contains nodes need to be masked and passed downstream.")
@@ -231,7 +231,7 @@ class GNNLearner:
 
 
 
-@knext.node(name="GNN Predictor", node_type=knext.NodeType.PREDICTOR, icon_path="icon.png", category="/")
+@knext.node(name="GNN Predictor", node_type=knext.NodeType.PREDICTOR, icon_path="../icons/icon.png", category="/")
 @knext.input_binary("GNN model", "The trained model", "org.knime.torch.learner")
 @knext.input_table(name="Test Set", description="A table that contains nodes for testing.")
 @knext.output_table("Table with Prediction", "Append prediction probability and class to table")
